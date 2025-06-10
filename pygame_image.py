653ed186846be19f4a7b31aaca159e9ad7d23c5d
raise <C0A24048ba@edu.teu.ac.jp>
2025-06-10 16:42:05 +0900

    練習10修正

--- pygame_image.py
+++ pygame_image.py
@@ -24,13 +24,13 @@ def main():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.movie_ip(0,-1)
+            kk_rct.move_ip((0,-1))
         if key_lst[pg.K_DOWN]:
-            kk_rct.movie_ip(0,+1)
+            kk_rct.move_ip((0,+1))
         if key_lst[pg.K_LEFT]:
-            kk_rct.movie_ip(-1,0)
+            kk_rct.move_ip((-1,0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.movie_ip(+1,0)
+            kk_rct.move_ip((+1,0))
         
 
         x=tmr%3200
@@ -43,6 +43,7 @@ def main():
         pg.display.update()
         tmr += 1        
         clock.tick(200)
+        # print(key_list)
 
         
 
